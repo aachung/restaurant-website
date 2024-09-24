08adb3de628127a198af1e2b7acc55fd683a8d78
Annie Chung <annie.chung@jostle.me>
2024-09-23 19:03:15 -0700

    comp1850 final project

---
+++ scripts/slick.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.multiple-items').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		dots: true,
+	  });
+  });
+
+  
